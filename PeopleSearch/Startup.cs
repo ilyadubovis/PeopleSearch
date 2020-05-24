588bffdeca428b60ce5daafe4d19487c3c9f2c7c
ilyadubovis <65792402+ilyadubovis@users.noreply.github.com>
2020-05-24 11:45:43 -0500

    Migration application on startup added.

--- PeopleSearch/Startup.cs
+++ PeopleSearch/Startup.cs
@@ -36,17 +36,20 @@ namespace PeopleSearch
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, AppDbContext dbContext)
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage();;
             }
             else
             {
                 app.UseExceptionHandler("/Error");
             }
 
+            // create tables and seed data by applying migration
+            dbContext.Database.Migrate();
+            
             app.UseStaticFiles();
             if (!env.IsDevelopment())
             {
